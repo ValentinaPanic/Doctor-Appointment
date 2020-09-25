b63ff2dd1b7c763a30a8c0cec5354a8ce68bcbe9
ValentinaPanic <valentina.panic87@gmail.com>
2020-09-25 17:45:47 -0600

    created patients full crud, validations for appointments and error full messages

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,29 +1,56 @@
 class AppointmentsController < ApplicationController
+    before_action :set_appointment, only: [:show, :update, :edit, :destroy]
+
     def index
         @appointments = Appointment.all
     end
 
 
     def new
-
+        @appointment = Appointment.new
     end
 
 
     def create
-
+        @appointment = Appointment.new(app_params)
+        if @appointment.save
+            redirect_to appointment_path(@appointment)
+        else
+             render :new
+        end
     end
+
 
 
     def show
+        
+    end
+
+
+    def edit
 
     end
+
 
     def update
-
+        @appointment.update(app_params)
+        redirect_to appointment_path(@appointment)
     end
 
 
-    def delete
-
+    def destroy
+        @appointment.destroy
+        redirect_to appointments_path
     end
+
+
+    private
+
+    def app_params
+        params.require(:appointment).permit(:patient_id, :doctor_id, :condition, :date)
+    end
+    def set_appointment
+        @appointment = Appointment.find(params[:id])
+    end
+
 end
